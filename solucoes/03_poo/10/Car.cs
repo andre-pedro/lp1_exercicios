72818e53c2c3bedba7396072316cdcbbc08acead
Nuno Figueiredo <nunosilvares@hotmail.com>
2018-06-01 16:08:09 +0100

    Adicionar solução do exercicio 03_10

---
+++ solucoes/03_poo/10/Car.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace LP1Exercises
+{
+    public enum FuelType { Gasoline, Diesel, LPG, Electric };
+
+    public class Car
+    {
+        private FuelType _Fuel;
+        private float _Weight;
+        private float _Speed;
+        public static float _MaxSpeed;
+
+        public float Speed
+        {
+            get { return _Speed; }
+            set { }
+        }
+
+        public float Weight
+        {
+            get { return _Weight; }
+            set { }
+        }
+
+        public FuelType Fuel
+        {
+            get { return _Fuel; }
+            set { }
+        }
+
+        public static float MaxSpeed
+        {
+            get { return _MaxSpeed; }
+            set { _MaxSpeed = MaxSpeed; }
+        }
+
+        public Car(float weight, float maxSpeed, FuelType fuel)
+        {
+            _Fuel = fuel;
+            _Weight = weight;
+            _MaxSpeed = maxSpeed;
+            _Speed = 0;
+        }
+
+        public float Accelerate(float x)
+        {
+            Console.WriteLine($"\tAccelerating {x}");
+
+            _Speed += x;
+
+            if (_Speed > MaxSpeed)
+            {
+                Console.WriteLine($"\t\tHitted Max Speed ({MaxSpeed}), accelerated {x - (_Speed - MaxSpeed)}");
+                _Speed = MaxSpeed;
+            }
+            return _Speed;
+        }
+
+        public float Break(float x)
+        {
+            Console.WriteLine($"\tBreaking {x}");
+
+            _Speed -= x;
+
+            if (_Speed < 0)
+            {
+                Console.WriteLine($"\t\tHitted Full Stop (0), breaked {_Speed + x}");
+                _Speed = 0;
+            }
+            return _Speed;
+        }
+    }
+}
---
+++ solucoes/03_poo/10/TestCar.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LP1Exercises
+{
+    class TestCar
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car(1000, 220, FuelType.Gasoline);
+
+            // Test GetFuelType, GetWeight , GetMaxSpeed
+            Console.WriteLine($"Car Fuel: {car.Fuel}");
+            Console.WriteLine($"Car Weight: {car.Weight}");
+            Console.WriteLine($"Car Max Speed: {Car.MaxSpeed}");
+            Console.WriteLine();
+            // Test Accelerate, Break and GetSpeed
+            Console.WriteLine($"Speed: {car.Speed}");
+            car.Accelerate(50);
+            Console.WriteLine($"Speed: {car.Speed}");
+            car.Accelerate(500);
+            Console.WriteLine($"Speed: {car.Speed}");
+            car.Break(50);
+            Console.WriteLine($"Speed: {car.Speed}");
+            car.Break(500);
+            Console.WriteLine($"Speed: {car.Speed}");
+            Console.WriteLine();
+            // Test SetMaxSpeeda
+            Car._MaxSpeed = 300;
+            Console.WriteLine($"Car Max Speed: {Car.MaxSpeed}");
+            car.Accelerate(500);
+        }
+    }
+}
