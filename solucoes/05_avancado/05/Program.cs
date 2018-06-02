b0cc78794bce108fd4adb480ba77406696e31fda
Nuno Figueiredo <nunosilvares@hotmail.com>
2018-06-02 20:20:27 +0100

    Alterações na solução do ex05_05

--- solucoes/05_avancado/05/Program.cs
+++ solucoes/05_avancado/05/Program.cs
@@ -4,18 +4,26 @@ namespace LP1Exercises
 {
     class Program
     {
-        readonly static double GoldenRatio = (1 + Math.Sqrt(5)) / 2;
-
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Choose a positive number and I will give you the respective Lucas number.");
-            double choice = Convert.ToDouble(Console.ReadLine());
-            // Only works until 1474, after that the number is too high
-            if (choice == 1) Console.WriteLine("Your Lucas number is 1.");
-            if (choice >= 2)
+            int choice = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Your Lucas Number is {Lucas(choice)}");
+        }
+
+        public static int Lucas(int choice)
+        {
+            if (choice == 0)
             {
-                Console.WriteLine($"Your Lucas number is " +
-                    $"{Math.Round(Math.Pow(GoldenRatio, choice), MidpointRounding.AwayFromZero)}.");
+                return 2;
+            }
+            if (choice == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return Lucas(choice - 1) + Lucas(choice - 2);
             }
         }
     }
