791669f4b01941c79a48f57453cded4ee726cd70
Nuno Figueiredo <nunosilvares@hotmail.com>
2018-06-02 20:43:05 +0100

    Adicionar Função local, remover método recursivo

--- solucoes/05_avancado/05/Program.cs
+++ solucoes/05_avancado/05/Program.cs
@@ -9,21 +9,11 @@ namespace LP1Exercises
             Console.WriteLine("Choose a positive number and I will give you the respective Lucas number.");
             int choice = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"Your Lucas Number is {Lucas(choice)}");
-        }
-
-        public static int Lucas(int choice)
-        {
-            if (choice == 0)
+            int Lucas(int n)
             {
-                return 2;
-            }
-            if (choice == 1)
-            {
-                return 1;
-            }
-            else
-            {
-                return Lucas(choice - 1) + Lucas(choice - 2);
+                if (n == 0) return 2;
+                if (n == 1) return 1;
+                else return Lucas(n - 1) + Lucas(n - 2);
             }
         }
     }
