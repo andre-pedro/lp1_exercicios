395b195afeddd5469e3801f986433d40b9974dcb
André Santos <afbsantos1@gmail.com>
2018-06-04 13:54:31 +0100

    ex7 05_avancados

---
+++ solucoes/05_avancado/07/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nums = 0;
+
+            Console.WriteLine("Indique a quantidade de numeros");
+            nums =  Convert.ToInt32(Console.ReadLine());
+            double[] numeros = new double[nums];
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                double tmp;
+                double[] copy = new double[numeros.Length];
+                Console.WriteLine("Escreva um numero");
+                tmp = Convert.ToInt32(Console.ReadLine());
+                numeros[i] = tmp;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Moda:{Stats.Moda(numeros)} Mediana:{Stats.Mediana(numeros)} " +
+                $"Média:{Stats.Media(numeros)} Máximo:{Stats.Maximo(numeros)} Minimo:{Stats.Minimo(numeros)}");
+
+        }
+    }
+}
---
+++ solucoes/05_avancado/07/Stats.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07
+{
+    static class Stats
+    {
+
+        public static double Media(double[] array)
+        {
+            double total = 0;
+            foreach (double num in array)
+            {
+                total += num;
+            }
+            return total / array.Length;
+        }
+
+        public static double Mediana(double[] array)
+        {
+            int size = array.Length;
+            int mid = size / 2;
+
+            double mediana = (size % 2 != 0) ? (double)array[mid] : (double)array[mid] + (double)array[mid - 1] / 2;
+            return mediana;
+
+        }
+
+        public static double Moda(double[] array)
+        {
+            double moda = 0 ;
+            foreach (double num in array)
+            {
+                if (num != 0 && moda != num)
+                {
+                    moda = num;
+                }
+            
+
+
+            }
+            return moda;
+        }
+
+        public static double Minimo(double[] array)
+        {
+            double min = array.Min();
+            
+            return min;
+        }
+
+        public static double Maximo(double[] array)
+        {
+            double max = array.Max();
+
+            return max;
+        }
+    }
+}
